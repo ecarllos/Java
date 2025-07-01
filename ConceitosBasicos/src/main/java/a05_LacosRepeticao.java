8de44d16821c2681b149219fa0d2fbc433a29fda
Eduardo <ecarllos017@gmail.com>
2025-07-01 18:20:41 -0300

    add file BREAK

--- ConceitosBasicos/src/main/java/a05_LacosRepeticao.java
+++ ConceitosBasicos/src/main/java/a05_LacosRepeticao.java
@@ -88,18 +88,48 @@ public class a05_LacosRepeticao {
         // de temperatura mais alta observada até aquele ponto. Você deve considerar a entrada como uma
         // sequência de valores inteiros de temperatura e imprimir o maior valor antes da condição de parada.
 
+
+        /*
         int maxTemperature = 0; // Initialize maximum temperature observed
-        int num = 0;
+
+        boolean ver = false;
 
         // Add your while loop or do-while loop here to process input temperatures
         while (maxTemperature<100){
             int temp = scanner.nextInt();
 
-
-            maxTemperature = temp;
+            if (temp>=100){
+                break;
+            }
+            if (!ver || temp > maxTemperature){
+                maxTemperature = temp;
+                ver = true;
+            }
         }
 
         // After exiting the loop, print the highest temperature
-        System.out.println(maxTemperature);
+        System.out.println(maxTemperature);*/
+
+
+
+        /*Como entrada, o programa recebe uma sequência de números inteiros não negativos;
+        cada número inteiro é escrito em uma linha separada. A sequência termina com um número inteiro 0;
+        quando o programa lê 0, ele deve encerrar seu trabalho e gerar o comprimento da sequência (sem contar o 0 final).*/
+
+        int cont = 0;
+        int n;
+        int c = 0;
+
+        do {
+            n = scanner.nextInt();
+            cont++;
+
+            if (n > c){
+                c = n;
+            }
+        } while (n != 0);
+
+        // System.out.println(cont-1);
+        System.out.println(c);
     }
 }
