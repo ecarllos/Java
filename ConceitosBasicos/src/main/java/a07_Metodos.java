1309f73c5ba59980c95043b36922056ce96f0a9c
Eduardo <ecarllos017@gmail.com>
2025-07-27 17:32:21 -0300

    iniciaÃlizando os metodos

---
+++ ConceitosBasicos/src/main/java/a07_Metodos.java
@@ -0,0 +1,11 @@
+public class a07_Metodos {
+    // Faça uma calculadora utilizando métodos em Java
+    public static void main(String[] args) {
+        String a = null;
+
+        if (a.equals("batata")) {
+            System.out.println("Ok");
+        }
+    }
+
+}
