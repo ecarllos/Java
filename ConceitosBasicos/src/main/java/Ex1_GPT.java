16a3560b42f256e34b7cb57598d332f1219878f1
Eduardo <ecarllos017@gmail.com>
2025-08-16 14:27:07 -0300

    Ex_GPT finalizado (Sitema Bancário)

--- ConceitosBasicos/src/main/java/Ex1_GPT.java
+++ ConceitosBasicos/src/main/java/Ex1_GPT.java
@@ -1,17 +1,18 @@
 import java.awt.*;
 import java.util.Scanner;
 
+
+
 public class Ex1_GPT {
     public static void main(String[] args) {
-        mostrar();
         funcao();
     }
 
+    static double saldo = 1524.56;
 
     public static void limparTela(){
-        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 50; i++){
+        for (int i = 0; i < 10; i++){
             System.out.println("");
         }
     }
@@ -21,8 +22,7 @@ public class Ex1_GPT {
         System.out.println("-----------BANCO-----------");
         System.out.println("1. Depositar Dinheiro");
         System.out.println("2. Sacar Dinheiro");
-        System.out.println("3. Consultar Saldo");
-        System.out.println("4. Sair");
+        System.out.println("3. Sair");
         System.out.println("");
 
         System.out.print("Digite a sua ação: ");
@@ -32,7 +32,7 @@ public class Ex1_GPT {
         Scanner scanner = new Scanner(System.in);
 
         mostrar();
-        double saldo = 1524.56;
+
 
         int option = scanner.nextInt();
 
@@ -46,23 +46,25 @@ public class Ex1_GPT {
                     System.out.print("Digite o quanto você irá depositar: ");
                     double add = scanner.nextDouble();
                     saldo += add;
-                    System.out.println("Saldo atual (R$:" + saldo + ")");
                     limparTela();
-
+                    System.out.println("Saldo atual (R$:" + saldo + ")");
+                    funcao();
                     break;
                 case 2:
                     System.out.println("Digite o quanto você irá sacar: ");
                     double saque = scanner.nextDouble();
                     saldo -= saque;
+                    limparTela();
                     System.out.println("Saldo atual (R$:" + saldo + ")");
+                    funcao();
                     break;
                 case 3:
-                    System.out.println("Saldo atual (R$:" + saldo + ")");
-                    break;
-                case 4:
                     System.out.println("Operação Finalizada!");
-                    option = 0;
                     break;
+                default:
+                    System.out.println("Digite um número correto!!");
+                    System.out.println("");
+                    funcao();
             }
 
         }
