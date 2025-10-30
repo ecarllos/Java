53e0eb15453712366b0a824f8559304655e1fccd
Eduardo <ecarllos017@gmail.com>
2025-10-30 06:46:00 -0300

    feat: add execiciospara relembrar arrays

---
+++ ConceitosBasicos/src/main/java/a08_Array.java
@@ -0,0 +1,68 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class a08_Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Crie uma matriz de longs chamada longN números com três elementos 100000000001, 100000000002, 100000000003.
+        // long[] longNumbers = {100000000001L, 100000000002L, 100000000003L};
+        // System.out.println(Arrays.toString(longNumbers));
+
+
+        /* Escreva um programa que realize as seguintes tarefas:
+        1. Aceita um inteiro 'n' como entrada, o que indica o número de elementos que devem estar no array.
+        2. Depois, aceita 'n' números inteiros como entrada para os elementos do array.
+        3. O programa deve então calcular a soma de todos os elementos no array que são múltiplos de 3.
+        4. Imprime a soma desses números como saída. */
+
+
+        // read the number of elements
+
+
+
+        /*int cont = scanner.nextInt();
+
+        // create your array here
+        int[] numbers = new int[cont];
+        int soma = 0;
+        // use a loop to read the array elements
+        for (int i = 0; i < cont; i++){
+            int n = scanner.nextInt();
+
+            if (n % 3 == 0){
+                numbers[i] = n;
+                soma += n;
+            }
+        }
+
+        System.out.println(soma);*/
+        // close the scanner
+
+
+
+
+        int[] array = new int[5];
+        int soma = 0;
+
+        for (int n : array){
+            soma += n;
+        }
+
+        System.out.println(soma);
+
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
