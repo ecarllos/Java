ee51d78ba03602e7f908e2fff1001044a3fc8d75
Eduardo <ecarllos017@gmail.com>
2025-10-31 19:10:50 -0300

    feat: add alicerce do código - guarda em uma array depois mostra ao user

--- ConceitosBasicos/src/main/java/ExA_GPT.java
+++ ConceitosBasicos/src/main/java/ExA_GPT.java
@@ -1,7 +1,27 @@
-import Scanner.util();
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ExA_GPT {
     public static void main(String[] args) {
         // Criar um mini-sistema de cadastro de tarefas (lista de tarefas) usando apenas console.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de Tarefas que você irá cadastrar: ");
+        int quant = scanner.nextInt();
+
+        String[] Tarefas = new String[quant];
+
+        for (int i = 0; i < quant; i++){
+            System.out.print("Digite o nome da Tarefa que vc quer cadastrar: ");
+            String Tarefa = scanner.next();
+
+            Tarefas[i] = Tarefa;
+        }
+
+        System.out.println("Essas são as suas Tarefas: " + Arrays.toString(Tarefas));
+
+        scanner.close();
     }
 }
